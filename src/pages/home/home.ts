acd15ad29dc3ad5f1d3b7a982813e49f36a3105b
Z <zsgithub@yahoo.com>
2017-07-24 11:32:41 -0400

    added left out code

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
-// import * as moment from 'moment';
+import * as moment from 'moment';
 
 @Component({
   selector: 'page-home',
@@ -24,7 +24,7 @@ export class HomePage {
   }
 
   check(age: any) {
-    if(age < 18) {
+    if(age < this.age) {
       this.checkit = "Not old enough";
     } else {
       this.checkit = "Old enough";
@@ -43,10 +43,26 @@ export class HomePage {
           "orientation" : "portrait" // Android only (portrait|landscape), default unset so it rotates with the device
         }
       ).then((barcodeData) => {
+        console.log(barcodeData.text);
+  	    console.log('stringified:');
+  	    console.log(JSON.stringify(barcodeData.text));
+  	    console.log(JSON.stringify(barcodeData));
+  	    this.scannedText = barcodeData.text;
+  	    this.scannedText = this.scannedText.split("\n");
+  	  	this.dob = this.scannedText[13].slice(3);
+  	  	this.name = this.scannedText[2].slice(3);
+  	  	this.height = this.scannedText[10].slice(3);
+  	  	this.formattedDob = moment(this.dob,"YYYY-MM-DD").format();
+  	  	this.age = moment().diff(this.formattedDob, 'years', true);
+  	  	this.check(this.age);
        // Success! Barcode data is here
       }, (err) => {
           // An error occurred
       });
+  }
+
+  reset(){
+    this.name = '';
   }
 
 }
